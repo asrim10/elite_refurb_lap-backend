d02af006c45fd2dd6e5dbfc3953a58c7733600a9
asrim10 <asrimmsuwall@gmail.com>
2026-04-18 20:16:08 +0545

    upload_middleware

---
+++ src/middlewares/upload.middleware.ts
@@ -0,0 +1,47 @@
+import multer from "multer";
+import { v4 as uuidv4 } from "uuid";
+import { Request } from "express";
+import path from "path";
+import { HttpError } from "../errors/http-error";
+import fs from "fs";
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    const uploadPath = path.join(__dirname, "../../uploads/");
+    if (!fs.existsSync(uploadPath)) {
+      fs.mkdirSync(uploadPath, { recursive: true });
+    }
+
+    cb(null, path.join(__dirname, "../../uploads/"));
+  },
+  filename: function (req, file, cb) {
+    const fileSuffix = uuidv4();
+    cb(null, fileSuffix + "-" + file.originalname);
+  },
+});
+
+const fileFilter = (
+  req: Request,
+  file: Express.Multer.File,
+  cb: multer.FileFilterCallback,
+) => {
+  if (file.mimetype.startsWith("image")) {
+    cb(null, true);
+  } else {
+    cb(new HttpError("Invalid file type, only JPEG and PNG is allowed!", 400));
+  }
+};
+
+const upload = multer({
+  storage: storage,
+  limits: { fileSize: 1024 * 1024 * 5 }, // 5MB
+  fileFilter: fileFilter,
+});
+
+export const uploads = {
+  single: (fieldName: string) => upload.single(fieldName),
+  array: (fieldName: string, maxCount: number) =>
+    upload.array(fieldName, maxCount),
+  fields: (fieldsArray: { name: string; maxCount?: number }[]) =>
+    upload.fields(fieldsArray),
+};
